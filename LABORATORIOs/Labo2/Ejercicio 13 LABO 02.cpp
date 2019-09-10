6324717a371f8f4671bfb8a9308d9a2df5736d26
Rodrigo Ernesto Mejia Galea <00037119gmail.com>
2019-09-10 02:07:51 -0600

    Agregar ejercicios de LABO 2 (10 Sept)

---
+++ LABORATORIOs/Labo2/Ejercicio 13 LABO 02.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/*
+
+		LA FUNCION HACE UNA SUMA DE TODOS LOS NUMEROS QUE COMPONEN AL NUMERO INGRESADO, HASTA LLEGAR AL MISMO NUMERO INGRESADO, ES DECIR
+		SI INGRESO EL NUMERO 3, LA FUNCION HACE UNA SUMA DE 1 + 2.. + N, HASTA LLEGAR A 3.
+
+int func(int n) {
+	if (n == 0)
+		return(0);
+	return(n + func(n - 1));
+}
+
+*/
+
+int FuncIterativa(int n) {
+
+	int suma = 0;
+	int digitos = 0;
+
+	if (n == 0) {
+		return 0;
+	}
+	else {
+		while (digitos <= n) {
+			suma += digitos;
+			digitos++;
+		}
+	}
+	return suma;
+
+}
+
+int main() {
+
+	int numero = 0, result = 0;
+
+	cout << "Ingrese el numero: ";
+	cin >> numero;	result = FuncIterativa(numero);
+	cout << "\n\nRESULTADO: " << result;
+}
---
+++ LABORATORIOs/Labo2/Ejercicio 2 LABO 02.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/*
+
+
+
+int func(int n) {
+	if (n == 0)
+		return(0);
+	return(n + func(n - 1));
+}
+
+*/
+
+
+int func(int n) {
+	if (n == 0)
+		return(0);
+	return(n + func(n - 1));
+}
+
+int main() {
+
+	int numero = 0, result = 0;
+	cin >> numero;	result=func(numero);
+	cout << "\n\nRESULTADO: " << result;
+}
